6b554c59e423361cbb350af60883ab0ae9502f2e
nipun0212 <nipumadan19@gmail.com>
2015-10-02 12:10:32 +0530

    Initializing girishICards

--- settings.py
+++ settings.py
@@ -12,7 +12,5 @@ created/forked from conference.py by wesc on 2014 may 24
 
 # Replace the following lines with client IDs obtained from the APIs
 # Console or Cloud Console.
-WEB_CLIENT_ID = 'replace with Web client ID'
-ANDROID_CLIENT_ID = 'replace with Android client ID'
-IOS_CLIENT_ID = 'replace with iOS client ID'
-ANDROID_AUDIENCE = WEB_CLIENT_ID
+WEB_CLIENT_ID = '327588008760-1nhea89p0o8dtd64pet2j0t6pg0hdph1.apps.googleusercontent.com'
+
--- static/js/app.js
+++ static/js/app.js
@@ -86,7 +86,7 @@ app.constant('HTTP_ERRORS', {
  */
 app.factory('oauth2Provider', function ($modal) {
     var oauth2Provider = {
-        CLIENT_ID: 'web-client-id',
+        CLIENT_ID: '327588008760-1nhea89p0o8dtd64pet2j0t6pg0hdph1.apps.googleusercontent.com',
         SCOPES: 'email profile',
         signedIn: false
     }
